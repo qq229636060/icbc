e32864b1d2e191f6f43627012942ed782601a5b3
Le Zheng <229636060@qq.com>
2020-08-31 16:53:31 +0800

    zll

--- src/views/infomsg.vue
+++ src/views/infomsg.vue
@@ -10,17 +10,18 @@
                   <div>交易时间</div><div>{{msg.pushed}}</div>
               </li>
               <li> <div>交易卡号</div><div>{{msg.card_no | formatcard1}}</div></li>
-              <li><div>交易账户</div><div>{{msg.acc}}</div></li>
+              <li><div>交易账户</div><div>{{msg.acc | formatcard1}}</div></li>
                 <li><div>交易户名</div><div>{{msg.user_name}}</div></li>
                  <li><div>业务摘要</div><div class="ss">{{msg.title}}<i></i></div></li>
-                  <li><div>交易国家或地区简称</div><div>{{msg.region}}</div></li>
-                   <li><div>交易场所</div><div>{{msg.title2}}</div></li>
+                  <li><div>交易国家或地区简称</div><div>CHN</div></li>
+                   <!-- <li><div>交易场所</div><div>{{msg.title2}}</div></li> -->
                     <li> <div>交易金额</div><div>{{msg.money}}</div></li>
                      <li><div>记账金额</div><div>{{msg.record}}</div></li>
-                      <li> <div>记账币种</div><div>{{msg.currency}}</div></li>
-                       <li :class="msg.opposing_name == ''? 'btt':''"><div>交易卡余额</div><div>{{msg.balance}}</div></li>
+                      <li> <div>记账币种</div><div>人民币</div></li>
+                       <li :class="msg.other_bank == ''? 'btt':''"><div>交易卡余额</div><div>{{msg.balance}}</div></li>
                         <li class="tt" v-if="msg.opposing_acc !=''"><div>对方账户</div><div class="bold">{{msg.opposing_acc}}</div></li>
-                        <li class="btt" v-if="msg.opposing_name !=''"><div>对方户名</div><div class="bold">{{msg.opposing_name}}</div></li>
+                        <li class="" v-if="msg.opposing_name !=''"><div>对方户名</div><div class="bold">{{msg.opposing_name}}</div></li>
+                        <li class="btt" v-if="msg.other_bank !=''"><div>对方账户行别</div><div class="bold">{{msg.other_bank}}</div></li>
           </ul>
       </div>
   </div>
